bb61558212d3b65d6f44b26b1cabbf35d8ffd222
Samuel McQuillin <sammcq@gmail.com>
2016-03-03 14:32:15 -0600

    changed merge fxn

--- nmrcsurvey/merge.R
+++ nmrcsurvey/merge.R
@@ -4,7 +4,7 @@ df1<-read.csv(file=file.choose(),header=TRUE)
 	#second create df2 program data frame 
 df2<-read.csv(file=file.choose(),header=TRUE)
 	#finally, merge the data 
-test1<-merge(x=df1,y=df2[,1:236],by="Response.ID",all.x=TRUE)
+test1<-merge(x=df2[,1:236],y=df1,by="Response.ID",all=TRUE)
 
 #test git push from rsudio
 a<-1
